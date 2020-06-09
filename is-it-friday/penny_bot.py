3910d87efccb332688c8243f820be828026a8342
GlebVinnychenko <hv275@cam.ac.uk>
2020-06-09 12:55:59 +0100

    Added an exclusive file for the pennying bot

---
+++ is-it-friday/penny_bot.py
@@ -0,0 +1,25 @@
+import os
+import datetime
+import random
+
+import discord
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+CHANNEL = os.getenv('DISCORD_CHANNEL')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+client.run(TOKEN)
