d7eb582039dcfce68804e0fc148a039a6322a7fb
GlebVinnychenko <hv275@cam.ac.uk>
2020-06-09 13:44:26 +0100

    added the fuctionality of adding players to the game

--- is-it-friday/penny_bot.py
+++ is-it-friday/penny_bot.py
@@ -4,13 +4,15 @@ import random
 
 import discord
 from dotenv import load_dotenv
+from penny import Session
+from penny import Player
 
-
+#normal bot code, nicked your past one
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 CHANNEL = os.getenv('DISCORD_CHANNEL')
-
+sesh = Session()
 client = discord.Client()
 
 
@@ -21,5 +23,23 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+@client.event
+
+#reacts to a join request to add player to a session
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == 'join the fun':
+        await pennyjoin(message.channel)
+
+async def pennyjoin(channel):
+    message = await channel.fetch_message(channel.last_message_id)
+    print(f"\n{message.author} has joined the session")
+    newplayer=Player(message.author)
+    sesh.add_player(newplayer)
+    await channel.send(f"{message.author} is ready to penny")
+    print(sesh.players)
 
 client.run(TOKEN)
